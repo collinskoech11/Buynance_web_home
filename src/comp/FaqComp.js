06ab46a9d738eea3d27de28110178098db7eb9f6
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2023-03-07 14:32:32 +0300

    update text alignment

--- src/comp/FaqComp.js
+++ src/comp/FaqComp.js
@@ -9,7 +9,7 @@ const FaqComp = () => {
 	const data = [
 
 		{"key:":1, "ques":"How to Complete Identity Verification?" ,"ans":"Verify your Buynance account with any of the supported documents by following this guide."},
-		{"key:":2,"ques":"How to Buy Cryptocurrency on BUynance P2P?" ,"ans":"You can now buy cryptocurrencies using Indian Rupees with 0 transaction fees on Buynance P2P! Follow this guide for Weband App."},
+		{"key:":2,"ques":"How to Buy Cryptocurrency on Buynance P2P?" ,"ans":"You can now buy cryptocurrencies using Indian Rupees with 0 transaction fees on Buynance P2P! Follow this guide for Weband App."},
 		{"key:":3,"ques":"How to Complete Identity Verification?" ,"ans":"Verify your Buynance account with any of the supported documents by following this guide."},
 		{"key:":4,"ques":"How to Complete Identity Verification?" ,"ans":"Verify your Buynance account with any of the supported documents by following this guide."},
 		{"key:":5,"ques":"How to Complete Identity Verification?" ,"ans":"Verify your Buynance account with any of the supported documents by following this guide."},
